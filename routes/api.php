53a988232ff2b9898efe3a4f920f798cf4a1e992
Muhammad Solikhin <school.muhammad.solikhin@gmail.com>
2025-02-13 11:44:52 +0700

    Add API routes for posts and categories, and move logout route inside auth middleware

--- routes/api.php
+++ routes/api.php
@@ -7,11 +7,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-
+Route::apiResource('posts', PostController::class);
+Route::apiResource('categories', CategoryController::class);
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-    Route::apiResource('posts', PostController::class);
-    Route::apiResource('categories', CategoryController::class);
+        
 });
